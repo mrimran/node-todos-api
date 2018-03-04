8da9fc657d89ecc037bd58db3a444f3575313df1
imran <mrimranzahoor@gmail.com>
2018-03-05 01:20:39 +0500

    Modified the server.js and added Todo + User model along with schema.

--- server/server.js
+++ server/server.js
@@ -6,24 +6,48 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     }, 
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }, 
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
 var newTodo = new Todo({
-    text: 'Cook dinner',
+    text: 'Cook dinner 1',
     completed: true,
-    completedAt: 20180305
+    completedAt: 20180304
 });
 
 newTodo.save().then((doc) => {
     console.log('Saved todo', doc);
 }, (e) => {
     console.log('Unable to save todo', e);
-})
+});
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+});
+
+var newUser = new User({
+    email: 'testit@test.com'
+});
+
+newUser.save().then((doc) => {
+    console.log('User saved: ', doc);
+}, (e) => {
+    console.log('Unable to save user', e);
+});
