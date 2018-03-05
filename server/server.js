d3c7006959ff96aee8ccf928139f0593b5383b90
Imran <imran@zeropoint.it>
2018-03-05 19:58:44 +0500

    Added todos endpoint on POST + refactored code adding express and body-parser packages.

--- server/server.js
+++ server/server.js
@@ -1,53 +1,29 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-mongoose.Promise = global.Promise;//configuring to use promises
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var app = express();
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    }, 
-    completed: {
-        type: Boolean,
-        default: false
-    }, 
-    completedAt: {
-        type: Number,
-        default: null
-    }
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    //console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
-var newTodo = new Todo({
-    text: 'Cook dinner 1',
-    completed: true,
-    completedAt: 20180304
-});
+var port = 3000;
 
-newTodo.save().then((doc) => {
-    console.log('Saved todo', doc);
-}, (e) => {
-    console.log('Unable to save todo', e);
-});
-
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
-});
-
-var newUser = new User({
-    email: 'testit@test.com'
-});
-
-newUser.save().then((doc) => {
-    console.log('User saved: ', doc);
-}, (e) => {
-    console.log('Unable to save user', e);
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
 });
