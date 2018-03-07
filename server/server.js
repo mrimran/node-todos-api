b9426843e00704733390015588e9fdddeb71804b
Imran <imran@zeropoint.it>
2018-03-07 18:54:20 +0500

    Added PATCH /todos/:id endpoint.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var {ObjectId} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -60,6 +61,31 @@ app.delete('/todos/:id', (req, res) => {
         }
 
         res.status(200).send({todo});
+    }).catch((e) => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    var body = _.pick(req.body, ['text', 'completed']);//only allow updating these two values
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
     }).catch((e) => res.status(400).send());
 });
 
