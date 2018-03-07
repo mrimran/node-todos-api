00475edc4910d3ef9c36f184a643b4686967b7c4
Imran <imran@zeropoint.it>
2018-03-07 13:20:32 +0500

    Added DELETE /todos/:id endpoint.

--- server/server.js
+++ server/server.js
@@ -45,7 +45,23 @@ app.get('/todos/:id', (req, res) => {
 
         res.status(200).send({todo});
     }).catch((e) => console.log(e));
-})
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.status(200).send(todo);
+    }).catch((e) => res.status(400).send());
+});
 
 var port = process.env.PORT || 3000;
 
