4abccac5b060aad0044e2fbe17d46726bb6fb648
imran <mrimranzahoor@gmail.com>
2018-03-10 23:23:21 +0500

    Added tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -213,8 +213,8 @@ describe('POST /users', () => {
             User.findOne({email}).then((user) => {
                 expect(user).toBeDefined();
                 expect(user.password).not.toBe(pass);//the password should be hashed so shouldn't match
-            });
-            done();
+                done();
+            }).catch((e) => done(e));;
         });
     });
 
@@ -239,4 +239,55 @@ describe('POST /users', () => {
         .expect(400)
         .end(done);
     });
-})
+});
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+        request(app)
+        .post('/users/login')
+        .send({
+            email: users[1].email,
+            password: users[1].password
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.headers['x-auth']).toBeDefined();
+        })
+        .end((err, res) => {
+            if(err) {
+                return done(err);
+            }
+
+            User.findById(users[1]._id).then((user) => {
+                expect(user.tokens[0]).toMatchObject({
+                    access: 'auth', 
+                    token: res.headers['x-auth']
+                });
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+
+    it('should reject invalid login', (done) => {
+        request(app)
+        .post('/users/login')
+        .send({
+            email: users[1].email,
+            password: users[1].password+'1'//made password invalid
+        })
+        .expect(400)
+        .expect((res) => {
+            expect(res.headers['x-auth']).not.toBeDefined();
+        })
+        .end((err, res) => {
+            if(err) {
+                return done(err);
+            }
+
+            User.findById(users[1]._id).then((user) => {
+                expect(user.tokens.length).toBe(0);//there shouldn't be any token for invalid login
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+});
