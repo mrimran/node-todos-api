3d27f6384a6f86987bc7336f62711d4770d418b1
Imran <imran@zeropoint.it>
2018-04-05 18:35:35 +0500

    Async/Await was added to couple of methods in server.js

--- server/server.js
+++ server/server.js
@@ -57,23 +57,27 @@ app.get('/todos/:id', authenticate, (req, res) => {
     }).catch((e) => console.log(e));
 });
 
-app.delete('/todos/:id', authenticate, (req, res) => {
+app.delete('/todos/:id', authenticate, async (req, res) => {
     var id = req.params.id;
 
     if(!ObjectId.isValid(id)) {
         return res.status(404).send();
     }
 
-    Todo.findOneAndRemove({
-        _id: id,
-        _creator: req.user._id
-    }).then((todo) => {
+    try {
+        const todo = await Todo.findOneAndRemove({
+            _id: id,
+            _creator: req.user._id
+        });
+    
         if(!todo) {
             return res.status(404).send();
         }
-
+    
         res.status(200).send({todo});
-    }).catch((e) => res.status(400).send());
+    } catch (e) {
+        res.status(400).send()
+    }
 });
 
 app.patch('/todos/:id', authenticate, (req, res) => {
@@ -105,41 +109,43 @@ app.patch('/todos/:id', authenticate, (req, res) => {
 });
 
 //POST /users
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     console.log(body);
     var user = new User(body);
 
-    user.save().then(() => {
-        return user.generateAuthToken();
-    }).then((token) => {
+    try {
+        await user.save();
+        const token = await user.generateAuthToken();
         res.header('x-auth', token).send(user);
-    }).catch((e) => res.status(400).send(e));
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
-app.post('/users/login', (req, res) => {
-    var body = _.pick(req.body, ['email', 'password']);
-    User.findByCredentials(body.email, body.password).then((user) => {
-        //res.status(200).send(user);
-        return user.generateAuthToken().then((token) => {
-            res.header('x-auth', token).send(user);
-        });
-    }).catch((e) => {
+app.post('/users/login', async (req, res) => {
+    try {
+        const body = _.pick(req.body, ['email', 'password']);
+        const user = await User.findByCredentials(body.email, body.password);
+        const token = await user.generateAuthToken();
+        res.header('x-auth', token).send(user);
+    } catch (e) {
         res.status(400).send();
-    });
+    }
 });
 
 //TODO: .update doesn't work for now, continue on this when there is reply
-app.delete('/users/me/token', authenticate, (req, res) => {
-    req.user.removeToken(req.token).then(() => {
+app.delete('/users/me/token', authenticate, async (req, res) => {
+    try {
+        await req.user.removeToken(req.token);
         res.status(200).send();
-    }, () => {//if things don't go well, going to be fired when errors.
+    } catch (e) {
         res.status(400).send();
-    });
+    }
 });
 
 var port = process.env.PORT || 3000;
